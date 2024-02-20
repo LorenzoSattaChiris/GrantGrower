b006c3b3b5f0b6b15dadf040b51bece7a938988c
DevLorenzo <lorenzo.udemy.dev@gmail.com>
2024-02-20 19:09:40 +0000

    Uploading the Structure

--- public/javascript/home.js
+++
@@ -1,6 +0,0 @@
-var button = document.querySelector('.action_button');
-
-button.addEventListener('click', function() {
-    alert('Coming Soon');
-    // window.open('https://www.example.com', '_blank');
-});
